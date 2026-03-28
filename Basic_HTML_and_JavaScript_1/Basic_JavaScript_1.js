4a4da7dc34e52423c462b03548dfa8652a5e881a
unitx01 <dinxmoshxgolad@gmail.com>
2026-03-27 17:16:15 -0700

    Main

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+function My_First_Function() {
+        var String = "Kiss me , I'm fat";
+        var result = string.fontcolor("green");
+        document.getElementById("Fat").innerHTML =
+        result;
+}
+		
